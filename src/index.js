9434cde192558be09f681efa9b677f0b74fbdacf
Rita Nkem Daniel <ritankemdaniel@gmail.com>
2024-01-01 23:14:47 +0100

    Add pizza

--- src/index.js
+++ src/index.js
@@ -1,17 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+// import { pizzaData } from "./data";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <Pizza />
+      <Pizza />
+      <Footer />
+    </div>
+  );
+};
+
+const Pizza = () => {
+  return (
+    <div>
+      <img src="/pizzas/margherita.jpg" alt="pizza margherita" />
+      <h2>Salamino</h2>
+      <p>Tomatoes</p>
+    </div>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
